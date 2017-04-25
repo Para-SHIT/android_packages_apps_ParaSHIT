DirtyUnicorns/android_packages_apps_DU-Tweaks

Pulse: Moar bars for solid line renderer [3/3]
Change-Id: I6916da0d20994c54abf13d3c9d83130bf51f251d

src/com/dirtyunicorns/dutweaks/fragments/PulseSettings.java
@@ -47,6 +47,7 @@ public class PulseSettings extends SettingsPreferenceFragment implements
     private static final String SOLID_FUDGE = "pulse_solid_fudge_factor";
     private static final String SOLID_LAVAMP_SPEED = "lavamp_solid_speed";
     private static final String FADING_LAVAMP_SPEED = "fling_pulse_lavalamp_speed";
+    private static final String PULSE_SOLID_UNITS_COUNT = "pulse_solid_units_count";
 
     SwitchPreference mShowPulse;
     ListPreference mRenderMode;
@@ -60,6 +61,7 @@ public class PulseSettings extends SettingsPreferenceFragment implements
     CustomSeekBarPreference mSolidFudge;
     CustomSeekBarPreference mSolidSpeed;
     CustomSeekBarPreference mFadingSpeed;
+    CustomSeekBarPreference mSolidCount;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -144,6 +146,13 @@ public class PulseSettings extends SettingsPreferenceFragment implements
                 (CustomSeekBarPreference) findPreference(FADING_LAVAMP_SPEED);
         mFadingSpeed.setValue(fspeed);
         mFadingSpeed.setOnPreferenceChangeListener(this);
+
+        int count = Settings.Secure.getIntForUser(getContentResolver(),
+                Settings.Secure.PULSE_SOLID_UNITS_COUNT, 64, UserHandle.USER_CURRENT);
+        mSolidCount =
+                (CustomSeekBarPreference) findPreference(PULSE_SOLID_UNITS_COUNT);
+        mSolidCount.setValue(count);
+        mSolidCount.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -214,6 +223,11 @@ public class PulseSettings extends SettingsPreferenceFragment implements
             int val = (Integer) newValue;
             Settings.Secure.putIntForUser(getContentResolver(),
                     Settings.Secure.FLING_PULSE_LAVALAMP_SPEED, val, UserHandle.USER_CURRENT);
+            return true;
+        } else if (preference == mSolidCount) {
+            int val = (Integer) newValue;
+            Settings.Secure.putIntForUser(getContentResolver(),
+                    Settings.Secure.PULSE_SOLID_UNITS_COUNT, val, UserHandle.USER_CURRENT);
             return true;
         }
         return false;

Para-SHIT/android_packages_apps_ParaSHIT

src/in/parashit/fragments/PulseSettings.java
@@ -50,6 +50,7 @@ public class PulseSettings extends SettingsPreferenceFragment
     private static final String SOLID_FUDGE = "pulse_solid_fudge_factor";
     private static final String SOLID_LAVAMP_SPEED = "lavamp_solid_speed";
     private static final String FADING_LAVAMP_SPEED = "fling_pulse_lavalamp_speed";
+    private static final String PULSE_SOLID_UNITS_COUNT = "pulse_solid_units_count";
 
     SwitchPreference mShowPulse;
     ListPreference mRenderMode;
@@ -63,6 +64,7 @@ public class PulseSettings extends SettingsPreferenceFragment
     SeekBarPreference mSolidFudge;
     SeekBarPreference mSolidSpeed;
     SeekBarPreference mFadingSpeed;
+    SeekBarPreference mSolidCount;
 
     @Override
     protected int getMetricsCategory() {
@@ -152,6 +154,13 @@ public class PulseSettings extends SettingsPreferenceFragment
                 (SeekBarPreference) findPreference(FADING_LAVAMP_SPEED);
         mFadingSpeed.setValue(fspeed);
         mFadingSpeed.setOnPreferenceChangeListener(this);
+
+        int count = Settings.Secure.getIntForUser(getContentResolver(),
+                Settings.Secure.PULSE_SOLID_UNITS_COUNT, 64, UserHandle.USER_CURRENT);
+        mSolidCount =
+                (SeekBarPreference) findPreference(PULSE_SOLID_UNITS_COUNT);
+        mSolidCount.setValue(count);
+        mSolidCount.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -222,6 +231,11 @@ public class PulseSettings extends SettingsPreferenceFragment
             int val = (Integer) newValue;
             Settings.Secure.putIntForUser(getContentResolver(),
                     Settings.Secure.FLING_PULSE_LAVALAMP_SPEED, val, UserHandle.USER_CURRENT);
+            return true;
+        } else if (preference == mSolidCount) {
+            int val = (Integer) newValue;
+            Settings.Secure.putIntForUser(getContentResolver(),
+                    Settings.Secure.PULSE_SOLID_UNITS_COUNT, val, UserHandle.USER_CURRENT);
             return true;
         }
         return false;